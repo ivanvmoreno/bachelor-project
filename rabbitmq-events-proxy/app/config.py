799b81017c81028754dbfcbf458c7ac04e9f14f7
ivanvmoreno <contacto@ivanmoreno.me>
2020-07-23 11:41:52 +0200

    [bugfix] String quotes consistency

--- rabbitmq-events-proxy/app/config.py
+++ rabbitmq-events-proxy/app/config.py
@@ -2,16 +2,16 @@ import base64
 from os import getenv
 
 # Namespace where kubeless is deployed
-KUBELESS_NAMESPACE = getenv("KUBELESS_NAMESPACE")
+KUBELESS_NAMESPACE = getenv('KUBELESS_NAMESPACE')
 
 # RabbitMQ cluster connection URI
-RABBITMQ_URI = getenv("RABBITMQ_CONNECTION_URI")
+RABBITMQ_URI = getenv('RABBITMQ_CONNECTION_URI')
 
 # Default exchange name for shared queues binding
-RABBITMQ_EXCHANGE_NAME = getenv("RABBITMQ_EXCHANGE_NAME")
+RABBITMQ_EXCHANGE_NAME = getenv('RABBITMQ_EXCHANGE_NAME')
 
 # Path to the directory in which the triggers store volume is mounted
-TRIGGERS_VOLUME_PATH = getenv("TRIGGERS_PATH")
+TRIGGERS_VOLUME_PATH = getenv('TRIGGERS_PATH')
 
 # Filename of the triggers store at TRIGGERS_VOLUME_PATH/
 TRIGGERS_FILENAME = 'triggers_store'
