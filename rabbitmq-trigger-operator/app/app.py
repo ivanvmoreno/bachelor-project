1001873e6e64bb59650a04e753f680a6866cdff5
ivanvmoreno <contacto@ivanmoreno.me>
2020-07-30 11:24:00 +0200

    [feat] RabbitMQTrigger CRD group

--- rabbitmq-trigger-operator/app/app.py
+++ rabbitmq-trigger-operator/app/app.py
@@ -15,34 +15,34 @@ def operator_startup(logger, **_):
   api = kubernetes.client.CoreV1Api()
 
   try:
-    api.read_namespaced_secret(namespace=NAMESPACE_NAME, name=TRIGGERS_STORE_SECRET)
+    api.read_namespaced_secret(namespace=TRIGGERS_NAMESPACE, name=TRIGGERS_STORE_SECRET)
   except ApiException:
     api_custom_objects = kubernetes.client.CustomObjectsApi()
 
     # Get all RabbitMQTrigger objects in the cluster (returns list)
     triggers = api_custom_objects.list_namespaced_custom_object(
-      TRIGGER_OBJECT_GROUP,
-      TRIGGER_OBJECT_API_VERSION,
-      NAMESPACE_NAME, 
-      TRIGGER_OBJECT_PLURAL)
+      TRIGGERS_GROUP,
+      TRIGGERS_API_VERSION,
+      TRIGGERS_NAMESPACE,
+      TRIGGERS_PLURAL)
 
     # Marshall list of trigger objects into JSON
     triggers_store = json.dumps(dict(map(marshall_trigger, triggers)))
     body = api.V1Secret(
-      metadata=api.V1ObjectMeta(namespace=NAMESPACE_NAME, name=TRIGGERS_STORE_SECRET),
+      metadata=api.V1ObjectMeta(namespace=TRIGGERS_NAMESPACE, name=TRIGGERS_STORE_SECRET),
       data=dict((TRIGGERS_SECRET_KEY, triggers_store)))
 
     # Create k8s namespaced secret containing the marshalled list of triggers
-    api.create_namespaced_secret(NAMESPACE_NAME, body)
+    api.create_namespaced_secret(TRIGGERS_NAMESPACE, body)
 
   try:
-    api.read_namespaced_service(EVENTS_PROXY_SERVICE, NAMESPACE_NAME)
+    api.read_namespaced_service(EVENTS_PROXY_SERVICE, TRIGGERS_NAMESPACE)
   except ApiException:
     raise ServiceNotRunning(f'RabbitMQ events proxy service ({EVENTS_PROXY_SERVICE}) is not running')
 
 
 # New trigger object handler
-@kopf.on.create(TRIGGER_OBJECT_GROUP, TRIGGER_OBJECT_API_VERSION, TRIGGER_OBJECT_PLURAL)
+@kopf.on.create(TRIGGERS_GROUP, TRIGGERS_API_VERSION, TRIGGERS_PLURAL)
 def handle_new_trigger(spec, **_):
   # Initialize k8s client
   kubernetes.config.load_incluster_config()
@@ -54,18 +54,18 @@ def handle_new_trigger(spec, **_):
 
   try:
     # Read secret
-    secret = api.read_namespaced_secret(TRIGGERS_STORE_SECRET, NAMESPACE_NAME)
+    secret = api.read_namespaced_secret(TRIGGERS_STORE_SECRET, TRIGGERS_NAMESPACE)
 
     # Update secret contents
     updated_store = json.dumps({ exchange_topic: destination_function, **json.loads(secret.body['TRIGGERS_SECRET_KEY']) })
     secret.body = dict((TRIGGERS_SECRET_KEY, updated_store))
-    api.replace_namespaced_secret(TRIGGERS_STORE_SECRET, NAMESPACE_NAME, secret)
+    api.replace_namespaced_secret(TRIGGERS_STORE_SECRET, TRIGGERS_NAMESPACE, secret)
   except ApiException:
-    raise kopf.TemporaryError(f'Secret {TRIGGERS_STORE_SECRET} not found in the namespace {NAMESPACE_NAME}', delay=30)
+    raise kopf.TemporaryError(f'Secret {TRIGGERS_STORE_SECRET} not found in the namespace {TRIGGERS_NAMESPACE}', delay=30)
     
 
 # Deleted trigger object handler
-@kopf.on.delete(TRIGGER_OBJECT_GROUP, TRIGGER_OBJECT_API_VERSION, TRIGGER_OBJECT_PLURAL)
+@kopf.on.delete(TRIGGERS_GROUP, TRIGGERS_API_VERSION, TRIGGERS_PLURAL)
 def delete(spec, **_):
   # Initialize k8s client
   kubernetes.config.load_incluster_config()
@@ -76,14 +76,14 @@ def delete(spec, **_):
 
   try:
     # Read secret
-    secret = api.read_namespaced_secret(TRIGGERS_STORE_SECRET, NAMESPACE_NAME)
+    secret = api.read_namespaced_secret(TRIGGERS_STORE_SECRET, TRIGGERS_NAMESPACE)
   except ApiException:
-    raise kopf.TemporaryError(f'Secret {TRIGGERS_STORE_SECRET} not found in the namespace {NAMESPACE_NAME}', delay=30)
+    raise kopf.TemporaryError(f'Secret {TRIGGERS_STORE_SECRET} not found in the namespace {TRIGGERS_NAMESPACE}', delay=30)
 
   try:
     # Update secret contents
     updated_store = json.dumps(remove_trigger_store(exchange_topic, json.loads(secret.body['TRIGGERS_SECRET_KEY'])))
     secret.body = dict((TRIGGERS_SECRET_KEY, updated_store))
-    api.replace_namespaced_secret(TRIGGERS_STORE_SECRET, NAMESPACE_NAME, secret)
+    api.replace_namespaced_secret(TRIGGERS_STORE_SECRET, TRIGGERS_NAMESPACE, secret)
   except MissingTriggerStore as error:
     raise kopf.TemporaryError(f'Trigger (topic {exchange_topic}) not found in the trigger secret store {TRIGGERS_STORE_SECRET}', delay=30)
--- rabbitmq-trigger-operator/app/config.py
+++ rabbitmq-trigger-operator/app/config.py
@@ -1,19 +1,22 @@
 from os import getenv
 
-# Name of the namespace
-NAMESPACE_NAME = getenv('NAMESPACE_NAME')
+# Namespace grouping the project components and RabbitMQTrigger objects
+TRIGGERS_NAMESPACE = getenv('TRIGGERS_NAMESPACE')
 
 # Group of the trigger CRD
-TRIGGER_OBJECT_GROUP = getenv('TRIGGER_OBJECT_GROUP')
+TRIGGERS_GROUP = getenv('TRIGGERS_GROUP')
 
 # k8s ApiVersion of the trigger CRD
-TRIGGER_OBJECT_API_VERSION = getenv('TRIGGER_OBJECT_API_VERSION')
+TRIGGERS_VERSION = getenv('TRIGGERS_VERSION')
 
 # Plural name of the trigger CRD
-TRIGGER_OBJECT_NAME_PLURAL = getenv('TRIGGER_OBJECT_NAME_PLURAL')
+TRIGGERS_PLURAL = getenv('TRIGGERS_PLURAL')
 
 # Name of the secret containing the triggers store
-TRIGGERS_STORE_SECRET = getenv('TRIGGER_OBJECT_NAME_PLURAL')
+TRIGGERS_STORE_SECRET = getenv('TRIGGERS_PLURAL')
+
+# Key in which to store the triggers global state on the secret
+TRIGGERS_SECRET_KEY = 'triggers_store'
 
 # Service name of the RabbitMQ events proxy
-EVENTS_PROXY_SERVICE = getenv('TRIGGER_OBJECT_NAME_PLURAL')
+EVENTS_PROXY_SERVICE = getenv('EVENTS_PROXY_SERVICE')
